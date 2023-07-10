cb4c168741fb7b7c62e0822417ff13a693032027
Walker402 <136858464+Walker402@users.noreply.github.com>
2023-07-09 23:10:37 -0400

    Update Lab 7.c

--- Lab 7.c
+++ Lab 7.c
@@ -1,42 +1,33 @@
 #include <stdio.h>
 
-void bubbleSort(int a[], int len, int* count)
-{
-    for (int i = 0; i < len; i++)
-    {
-        int swapcount = 0;
-        for (int j = 0; j < (len - 1); j++)
-        {
-            if (a[j] > a[j + 1])
-            {
-                int temp = a[j];
-                a[j] = a[j + 1];
-                a[j + 1] = temp;
-                (*count)++;
-                swapcount++;
+void bubbleSort(int arr[], int n, int swaps[]) {
+    int i, j, temp;
+    for (i = 0; i < n-1; i++) {
+        for (j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                swaps[j]++;
             }
         }
-        printf("%d\n", swapcount);
     }
 }
 
-int main()
-{
+int main() {
     int arr[] = {97, 16, 45, 63, 13, 22, 7, 58, 72};
     int n = sizeof(arr) / sizeof(arr[0]);
-    int swapCount = 0;
+    int swaps[n];
+    for (int i = 0; i < n; i++) {
+        swaps[i] = 0;
+    }
 
-    printf("Original Array: ");
-    for (int i = 0; i < n; i++)
-        printf("%d ", arr[i]);
+    bubbleSort(arr, n, swaps);
 
-    bubbleSort(arr, n, &swapCount);
-
-    printf("\nSorted Array: ");
-    for (int i = 0; i < n; i++)
-        printf("a[%d] = %d\n", i, arr[i]);
-
-    printf("The number of swaps needed to sort were: %d\n", swapCount);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", swaps[i]);
+    }
+    printf("\n");
 
     return 0;
 }
